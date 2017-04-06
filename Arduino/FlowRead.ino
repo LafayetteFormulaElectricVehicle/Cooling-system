c759f7082fa17ccdb552460a2b77303117c14ee6
Xingyuan Guo <guox@lafayette.edu>
2017-04-06 15:52:19 -0400

    Create FlowRead.ino

---
+++ Arduino/FlowRead.ino
@@ -0,0 +1,75 @@
+//constants———————————————————————
+const int FLOW_PIN = 3;
+const int INTERVAL = 1000;
+
+//declaration——————————————————————-
+void detectFlowmeterRotation(int flow_pin);
+void calculateFlowmeterFrequency();
+void printInfoToSerial();
+
+boolean latch1_, latch2_ = false;
+
+unsigned int window_count_ = 0;
+unsigned long total_count_ = 0;
+
+unsigned int current_frequency_ = 0;
+
+//——————————————————————————
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+//——————————————————————————
+void loop() {
+  detectFlowmeterRotation(FLOW_PIN);
+  calculateFlowmeterFrequency();
+  //printInfoToSerial();
+}
+
+//definition ——————————————————————-
+void detectFlowmeterRotation(int flow_pin){
+  if(digitalRead(flow_pin))
+  latch1_=true;
+  if(!digitalRead(flow_pin))
+  latch2_=true;
+
+  if(latch1_ && latch2_){//only true after a rotation
+    latch1_ = false;
+    latch2_ = false;
+    window_count_++;
+    total_count_++;
+  }
+}
+void calculateFlowmeterFrequency(){
+
+  static unsigned long previous_millis;
+
+  if (millis() - previous_millis > INTERVAL) { //if within the rolling window INTERVAL
+    previous_millis = millis(); //save last time the clock updated in previous_millis
+    current_frequency_ = window_count_ * (1000/INTERVAL);
+    window_count_ = 0; //reset window counter, cumulative counters unaffected.
+
+    Serial.print("total_count_: ");
+    Serial.print(total_count_);
+    Serial.print(" | ");
+    Serial.print("window_count_: ");
+    Serial.print(current_frequency_);
+    Serial.println();
+    
+  }
+}
+
+void printInfoToSerial(){
+  static unsigned long previous_millis;
+
+  if (millis() - previous_millis > INTERVAL) { //if within the rolling window INTERVAL
+    previous_millis = millis(); //save last time the clock updated in previous_millis
+    Serial.print("total_count_: ");
+    Serial.print(total_count_);
+    Serial.print(" | ");
+    Serial.print("window_count_: ");
+    Serial.print(current_frequency_);
+    Serial.println();
+  }
+}
